c619494cf98861c4f2312e7ef31e9a12c735cd82
Jakub Slocki <jakub@slocki.net>
2017-11-29 09:16:15 +0100

    update migrate's mysql driver to work with golang driver changes

--- driver/mysql/mysql.go
+++ driver/mysql/mysql.go
@@ -52,11 +52,7 @@ func (driver *Driver) Close() error {
 }
 
 func (driver *Driver) ensureVersionTableExists() error {
-	_, err := driver.db.Exec("CREATE TABLE IF NOT EXISTS " + tableName + " (version int not null primary key);")
-
-	if _, isWarn := err.(mysql.MySQLWarnings); err != nil && !isWarn {
-		return err
-	}
+	driver.db.Exec("CREATE TABLE IF NOT EXISTS " + tableName + " (version int not null primary key);")
 
 	return nil
 }
